620a25cafba6b8f61f464bb1297732f9817ca07f
kartikshingde <kartikshingde12@gmail.com>
2024-04-16 19:45:13 +0530

    all done for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,20 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import multer from "multer";
-
-import {upload} from "../middlewares/multer.middleware.js"
-
-
-const router = Router()
+import {upload} from "../middlewares/multer.middelware.js"
+const router=Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name: "avatar",
-            maxCount: 1
-        }, 
+            name:"avatar",
+            maxCount:1
+        },
         {
-            name: "coverImage",
-            maxCount: 1
+            name:"coverImage",
+            maxCount:1
         }
-    ]),
-    registerUser
-    )
 
+    ]),
+    registerUser)
 
 export default router
